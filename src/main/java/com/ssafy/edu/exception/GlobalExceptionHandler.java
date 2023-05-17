53359c46c58a0f4dc112c738dfb9807a347aff73
전영호 <aplblypt@gmail.com>
2023-05-17 15:44:45 +0900

    feat: globalExceptionHandler init

--- src/main/java/com/ssafy/edu/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssafy/edu/exception/GlobalExceptionHandler.java
@@ -1,5 +1,17 @@
 package com.ssafy.edu.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ssafy.edu.utils.ApiUtils;
+import com.ssafy.edu.utils.ApiUtils.ApiResult;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler
+	public ApiResult<?> IllegalException(IllegalArgumentException e) {
+		return ApiUtils.error("123", HttpStatus.ACCEPTED);
+	}
 }
