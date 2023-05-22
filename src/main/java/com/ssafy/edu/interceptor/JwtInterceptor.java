83aeb0d67f77a69db448ef51b6ef1304e2c3759f
JungInLee0130 <dlwjddls0130@naver.com>
2023-05-22 17:53:57 +0900

    feat: jwt 인증 interceptor 추가

--- src/main/java/com/ssafy/edu/interceptor/JwtInterceptor.java
+++ src/main/java/com/ssafy/edu/interceptor/JwtInterceptor.java
@@ -1,5 +1,8 @@
 package com.ssafy.edu.interceptor;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Objects;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,34 +10,66 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.ssafy.edu.exception.TokenExpiredException;
 import com.ssafy.edu.exception.UnAuthorizedException;
 import com.ssafy.edu.member.service.JwtService;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.swagger.models.HttpMethod;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
-
-	public static final Logger logger = LoggerFactory.getLogger(JwtInterceptor.class);
-
-	private static final String HEADER_AUTH = "auth-token";
-
-	@Autowired
-	private JwtService jwtService;
+	
+	private final JwtService jwtService;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		final String token = request.getHeader(HEADER_AUTH);
-
-		if (token != null && jwtService.checkToken(token)) {
-			logger.info("토큰 사용 가능 : {}", token);
+		if (isPreflightRequest(request)) {
+			return true;
+		}
+		String token = request.getHeader("access-token");
+		if (token == null) {
+			// 토큰이 존재하지않음
+			throw new UnAuthorizedException();
+		}
+		if (jwtService.checkToken(token)) {
 			return true;
 		} else {
-			logger.info("토큰 사용 불가능 : {}", token);
-			throw new UnAuthorizedException();
+			throw new TokenExpiredException();
 		}
 
 	}
+
+
+	private boolean isPreflightRequest(HttpServletRequest request) {
+		return isOptions(request) && hasHeaders(request) && hasMethod(request) && hasOrigin(request);
+	}
+
+	private boolean hasOrigin(HttpServletRequest request) {
+		return Objects.nonNull(request.getHeader("Origin"));
+	}
+
+	private boolean hasMethod(HttpServletRequest request) {
+		return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+	}
+
+	private boolean isOptions(HttpServletRequest request) {
+		return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+	}
+
+	private boolean hasHeaders(HttpServletRequest request) {
+		return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+	}
+
 
 }
