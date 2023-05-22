d330f25f0c25daffd828ca8932df28054132aee8
JungInLee0130 <dlwjddls0130@naver.com>
2023-05-22 09:20:47 +0900

    feat:jwt security 진행중

---
+++ src/main/java/com/ssafy/edu/security/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.ssafy.edu.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JwtAuthenticationFilter extends GenericFilterBean{
+	
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+		
+		if (token != null && jwtTokenProvider.validateToken(token)) {
+			// 토큰이 유효하면 토큰으로 
+			Authentication authentication = jwtTokenProvider.getAuthentication(token);
+			
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		chain.doFilter(request, response);
+	}
+	
+	
+}
