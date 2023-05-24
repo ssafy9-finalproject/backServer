3fc9994f40c90b1b5e63e9ebfd88b67bec0c8480
youngho9999 <aplblypt@gmail.com>
2023-05-24 21:20:48 +0900

    fix: member domain try catch 구문 다수 삭제, jwt exception 부분 수정

--- src/main/java/com/ssafy/edu/member/service/MemberService.java
+++ src/main/java/com/ssafy/edu/member/service/MemberService.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Service;
 import com.ssafy.edu.member.model.dto.MemberDto;
 
 public interface MemberService {
-	void join(MemberDto mdto) throws Exception;
+	void join(MemberDto mdto);
 	
-	MemberDto login(MemberDto mdto) throws Exception;
-	List<MemberDto> memberlist() throws Exception;
-	MemberDto memberDetail(String memberId) throws Exception;
-	void memberDelete(String memberId) throws Exception;
-	void memberUpdate(MemberDto mdto) throws Exception;
-	String findById(String memberId) throws Exception;
-	MemberDto loginMember(String memberId, String memberPassword) throws Exception;
+	MemberDto login(MemberDto mdto);
+	List<MemberDto> memberlist();
+	MemberDto memberDetail(String memberId);
+	void memberDelete(String memberId);
+	void memberUpdate(MemberDto mdto);
+	String findById(String memberId);
+	MemberDto loginMember(String memberId, String memberPassword);
 	
-	public void saveRefreshToken(String memberId, String refreshToken) throws Exception;
-	public Object getRefreshToken(String memberId) throws Exception;
-	public void deleRefreshToken(String memberId) throws Exception;
+	public void saveRefreshToken(String memberId, String refreshToken);
+	public Object getRefreshToken(String memberId);
+	public void deleRefreshToken(String memberId);
 }
