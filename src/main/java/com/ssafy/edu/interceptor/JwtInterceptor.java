ce27934965ea1b039c85349945b44733c77f3509
JungInLee0130 <dlwjddls0130@naver.com>
2023-05-24 17:52:44 +0900

    feat:jwt security 설정중

--- src/main/java/com/ssafy/edu/interceptor/JwtInterceptor.java
+++ src/main/java/com/ssafy/edu/interceptor/JwtInterceptor.java
@@ -26,31 +26,12 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
 	
-	//private final JwtService jwtService;
+	private final JwtService jwtService;
 	private final MemberService memberService;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-<<<<<<< HEAD
-//		if (isPreflightRequest(request)) {
-//			return true;
-//		}
-//		String token = request.getHeader("access-token");
-//		String refreshToken = request.getHeader("refresh-token");
-//		//log.debug("엑세스토큰 있음:{}",token);
-//		//log.debug("리프레시토큰 있음:{}",refreshToken);
-//		//log.debug("memberId 끌어올수있음:{}", jwtService.getMemberId(token));
-//		
-//		if (token == null && refreshToken == null) {
-//			throw new UnAuthorizedException();
-//		}
-//		if (jwtService.checkToken(token)) {
-//			return true;
-//		} 
-//		else if (jwtService.checkToken(refreshToken)) {
-//			// 토큰이 존재하지않음
-=======
 		if (isPreflightRequest(request)) {
 			return true;
 		}
@@ -72,23 +53,7 @@ public class JwtInterceptor implements HandlerInterceptor {
 			log.info("rignow: {}", ErrorCode.TOKEN_INVALID.getDescription());
 			throw new TokenInvalidException(ErrorCode.TOKEN_INVALID);
 		} 
-		else { // 둘다있고, 형식 맞음
-			return true;
-		}
-//		else if (jwtService.checkToken(refreshToken)) {
-//			// 토큰이 존재하지않거나 invalid함
->>>>>>> 70fdfe84bfb19f3243c1925c1b181b8a5fc67085
-//			jwtService.createAccessToken("memberId", jwtService.getMemberId(refreshToken));
-//			if (jwtService.checkToken(request.getHeader("access-token"))) {
-//				return true;
-//			}
-//		}
-<<<<<<< HEAD
-//		throw new TokenExpiredException();
 		return true;
-=======
-		
->>>>>>> 70fdfe84bfb19f3243c1925c1b181b8a5fc67085
 	}
 
 
